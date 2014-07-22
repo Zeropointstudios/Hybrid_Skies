4337b2e99fa067f151781d4d54523dc2810a821f
Pablo Leon-Luna <paleonluna@gmail.com>
2014-07-22 14:53:48 -0700

    got rid of excess assets and unused scripts

---
+++ Assets/Hybrid_Skies/Assets/Source/Scripts/Destroyer Scripts/DestroyByContact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+
+		Destroy (other.gameObject); // destroys bullet
+		Destroy (gameObject); //destroy asterioid (destroy destroys at end of frame, so order doesnt matter
+	}
+}
