8703a064c71f201d3a36b61d8584641ee36ed138
Pablo Leon-Luna <paleonluna@gmail.com>
2014-07-23 15:38:26 -0700

    more file structure changes

---
+++ Assets/Source/Scripts/HighLightTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighLightTarget : MonoBehaviour {
+	Color targetColor;
+	public Color highLightColor;
+	bool toggleHighlight = false;
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, Absorb.cameraDistance))
+		{
+
+		}
+
+	}
+}
+
