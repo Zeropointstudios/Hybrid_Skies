dd077bcccb7320db70125062b9b3cde838ff9f99
Skillpott <skillpott@gmail.com>
2014-07-30 13:33:27 -0700

    Follow cam updated. Ship texture updated. Haze texture created for testing.

---
+++ Assets/Source/Scripts/Controllers/SmoothFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour 
+
+{
+
+	public float LockedX = 0;
+	public float LockedY = 0;
+	public float LockedZ = 0;
+	
+	public GameObject Player;
+	
+	void Update()
+	{
+		transform.position = new Vector3(LockedX * Player.transform.position.x, LockedY, LockedZ);
+	}
+
+}
