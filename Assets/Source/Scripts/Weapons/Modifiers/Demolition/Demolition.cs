c59ad5bf97229ccaca8a58397c2f77712ca77f97
Pablo Leon-Luna <paleonluna@gmail.com>
2014-08-02 20:14:28 -0700

    added AOE scripts

---
+++ Assets/Source/Scripts/Weapons/Modifiers/Demolition/Demolition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demolition : MonoBehaviour {
+
+	public Collider splashCollider;
+	Collider splashInstance;
+
+	void OnTriggerEnter()
+	{
+		splashInstance = (Collider)Instantiate (splashCollider, gameObject.transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Source/Scripts/Weapons/Modifiers/Demolition/DemolitionPrefab.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemolitionPrefab : MonoBehaviour {
+
+	public int explosionDamage;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Destroy");
+	}
+
+	void OnTriggerEnter(GameObject other)
+	{
+		if (other.tag == "Enemy")
+		{
+			other.gameObject.GetComponent<HitPoints> ().doDamage (explosionDamage);
+		}
+	}
+
+	IEnumerator Destroy()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Destroy (gameObject);
+	}
+}
