c1cbee8bd530beb5506157a4e42a351bf53def42
aberrantspark <alphastorm@gmail.com>
2014-09-08 16:36:05 -0700

    A scene to test some particle FX... there are 2 in there right now, a poison effect  to the left and an absorb effect comprised of 2 effects to the center.

---
+++ Assets/Source/Scripts/Mover/Spinner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script can be used to simply spin an object of pre-defined speed at every fixed update.
+public class Spinner : MonoBehaviour {
+	public float fRotateSpeedX = 0;
+	public float fRotateSpeedY = 0;
+	public float fRotateSpeedZ = 0;
+	
+	Vector3 vRotate;
+	// Use this for initialization
+	void Start () {
+		vRotate.x = fRotateSpeedX;
+		vRotate.y = fRotateSpeedY;
+		vRotate.z = fRotateSpeedZ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (vRotate);
+	}
+	
+	void FixedUpdate(){
+		vRotate.x = fRotateSpeedX;
+		vRotate.y = fRotateSpeedY;
+		vRotate.z = fRotateSpeedZ;
+		
+		transform.Rotate (vRotate);
+	}
+}
