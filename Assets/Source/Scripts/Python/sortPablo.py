63ebd16dfa425a208507dc4dfc3683ca00a250ad
Aaron Smith <magicsquare15@gmail.com>
2014-09-09 02:25:24 -0700

    sortPablo.py now works for MacOS

--- Assets/Source/Scripts/Python/sortPablo.py
+++ Assets/Source/Scripts/Python/sortPablo.py
@@ -2,16 +2,21 @@ import os
 import shutil
 import sys
 
-UNITY_PROJECT_DIR = "C:\dev\AncientAliens"
-UNITY_SCENES_DIR = UNITY_PROJECT_DIR + "\Assets\Source\_Scenes";
-UNITY_PYTHON_DIR = UNITY_PROJECT_DIR + "\Assets\Source\Scripts\Python";
-UNITY_FILE = "Pablo.unity"
-UNITY_FILE_BACKUP_0 = UNITY_FILE + "_backup_0"
-UNITY_FILE_BACKUP_1 = UNITY_FILE + "_backup_1"
-UNITY_FILE_BACKUP_2 = UNITY_FILE + "_backup_2"
-UNITY_FILE_BACKUP_3 = UNITY_FILE + "_backup_3"
-UNITY_FILE_BACKUP_4 = UNITY_FILE + "_backup_4"
-UNITY_FILE_SORTED  = UNITY_FILE + "_sorted"
+if sys.platform == 'darwin':                                                  # For the Mac OS version...
+	UNITY_PROJECT_DIR = os.path.join(os.environ['HOME'], 'dev/AncientAliens') #   EDIT the root of your AncientAliens project.
+	UNITY_SCENES_DIR = UNITY_PROJECT_DIR + '/Assets/Source/_Scenes';       
+	UNITY_PYTHON_DIR = UNITY_PROJECT_DIR + '/Assets/Source/Scripts/Python'
+else:                                                                         # For the Windows verions...
+	UNITY_PROJECT_DIR = 'C:\dev\AncientAliens'                                #   EDIT the root of your AncientAliens project.
+	UNITY_SCENES_DIR = UNITY_PROJECT_DIR + '\Assets\Source\_Scenes';
+	UNITY_PYTHON_DIR = UNITY_PROJECT_DIR + '\Assets\Source\Scripts\Python'
+UNITY_FILE = 'Pablo.unity'	
+UNITY_FILE_BACKUP_0 = UNITY_FILE + '_backup_0'
+UNITY_FILE_BACKUP_1 = UNITY_FILE + '_backup_1'
+UNITY_FILE_BACKUP_2 = UNITY_FILE + '_backup_2'
+UNITY_FILE_BACKUP_3 = UNITY_FILE + '_backup_3'
+UNITY_FILE_BACKUP_4 = UNITY_FILE + '_backup_4'
+UNITY_FILE_SORTED  = UNITY_FILE + '_sorted'
 
 def do(action):
 	print(action)
@@ -42,9 +47,9 @@ def main(argv):
 	
 	shutil.copy(UNITY_FILE, UNITY_FILE_BACKUP_0);
 	
-	do("python %s\\sortScene.py %s >%s" % (UNITY_PYTHON_DIR, UNITY_FILE, UNITY_FILE_SORTED))
+	do('python %s %s >%s' % (os.path.join(UNITY_PYTHON_DIR, 'sortScene.py'), UNITY_FILE, UNITY_FILE_SORTED))
 
 	shutil.copy(UNITY_FILE_SORTED, UNITY_FILE);
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv)
