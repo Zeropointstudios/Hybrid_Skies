511376709ee06f565f884a21abea58abdb821aed
aberrantspark <alphastorm@gmail.com>
2014-09-15 15:23:46 -0700

    Cleaned up my test scene for particles so buttons toggle fx on/off and we can see how stacked fx looks and performs.

---
+++ Assets/Source/Scripts/PFX/TogglePFX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePFX : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Toggle()
+	{
+		if (particleSystem)
+			{
+			if (particleSystem.isStopped)
+			{
+				particleSystem.Play ();
+			}
+			else
+			{
+				particleSystem.Stop();
+			}
+		}
+	}
+}
