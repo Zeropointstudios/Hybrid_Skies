29ea4fbd335ca52d73732aa61f5029bcbdd99b49
aberrantspark <alphastorm@gmail.com>
2014-09-16 20:22:33 -0700

    Improvements to particle lab... am I pushing this right?

---
+++ Assets/Source/Scripts/PFX/PFX_Playall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PFX_Playall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayAll() {
+		foreach (Transform child in transform)
+		{
+			child.particleSystem.Play ();
+		}
+	}
+
+	public void StopAll() {
+		foreach (Transform child in transform)
+		{
+			child.particleSystem.Stop ();
+		}
+	}
+}
