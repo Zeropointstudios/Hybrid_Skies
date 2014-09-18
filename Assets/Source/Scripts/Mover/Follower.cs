6bba69173e5221c46a9a630c2f07c3e76acb10c5
aberrantspark <alphastorm@gmail.com>
2014-09-17 19:44:41 -0700

    Shield fx assets and elements.

--- Assets/Source/Scripts/Mover/Follower.cs
+++ Assets/Source/Scripts/Mover/Follower.cs
@@ -6,15 +6,31 @@ using System.Collections;
 public class Follower : MonoBehaviour {
 	public Transform tTarget;
 
+	public float fOffsetX = 0;
+	public float fOffsetY = 0;
+	public float fOffsetZ = 0;
+
+	Vector3 vOffset;
+	Vector3 vTargetPos;
+
+	void UpdatevOffset()
+	{
+		vOffset.x = fOffsetX;
+		vOffset.y = fOffsetY;
+		vOffset.z = fOffsetZ;
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		UpdatevOffset ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (tTarget) {
-			transform.position = tTarget.position;
+			UpdatevOffset ();
+			vTargetPos = tTarget.position + vOffset;
+			transform.position = vTargetPos;
 		}
 	}
 }
