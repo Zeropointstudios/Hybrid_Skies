76cbd8565c41f856fd6b375d7516c2d6df76d649
Pablo Leon-Luna <paleonluna@gmail.com>
2014-09-25 17:36:32 -0700

    added shields and armor calculation with pooling of their vex

--- Assets/Source/Scripts/Enemy/HitPoints.cs
+++ Assets/Source/Scripts/Enemy/HitPoints.cs
@@ -21,14 +21,18 @@ public class HitPoints : MonoBehaviour {
 
 	//TODO make enemies have a pool also
 	public GameObject deathVFX;
-	public GameObject defenseVFX;
 	ObjectPool squibPool;
 	public int squibID;
+	public int armorFXID;
+	public int shieldFXID;
 	public int hitPoints;
 	public int defense;   // damage done = damage - defense
+	public int maxShields;
+	int shields;
 	public ModifierType modifierType = ModifierType.None;
 	public RaceType raceType = RaceType.None;
-	public DefenseType defenseType = DefenseType.None;
+	public DefenseType defenseType = DefenseType.None; //pretty sure we decided that this does not matter or exist @Aaron from Pablo
+	public bool hasShields;
 
 	public ObjectPool returnSquibPool(){return squibPool;}
 	public int returnSquibID(){return squibID;}
@@ -36,17 +40,51 @@ public class HitPoints : MonoBehaviour {
 
 	void Awake() {
 		squibPool = GameObject.Find("SquibPool").GetComponent<ObjectPool>();
+		shields = maxShields;
+
+	}
+
+	IEnumerator ShieldRegeneration() {
+		while (hasShields)
+		{
+			yield return new WaitForSeconds(1);	//regenerates shields every second
+				if (shields < maxShields)
+					shields +=1;
+		}
 	}
 
 	public void doDamage(int damage)
 	{
-		int effectiveDamage = Mathf.Max(damage - defense, 0);
+		//if there are shields take away from them first
+		if (hasShields) {
+			if (shields >= damage) {
+				shields -= damage;
+				squibPool.Activate(shieldFXID, transform.position, Quaternion.identity);
+			}
 
-		if ( defenseVFX != null && defense > .5 * damage ) {
-			Instantiate(defenseVFX, transform.position, Quaternion.identity);
+			else
+				damage -= shields;
+				shields = 0;
+
 		}
 
-		hitPoints -= effectiveDamage;
+		if (defense > 0) {
+			int effectiveDamage = Mathf.Max(damage - defense, 0);
+
+			if (defense > .5 * damage ) {	//shows armor hits if the weapon is doing less than half its original damage
+				squibPool.Activate(armorFXID, transform.position, Quaternion.identity); //shows armor vfx hit
+			}
+
+			else
+				squibPool.Activate(squibID, transform.position, Quaternion.identity); //shows normal vfx hit
+			hitPoints -= effectiveDamage;
+
+		}
+
+		else //if there is no armor or shields, do what is left of damage (including reduction from shield)
+			squibPool.Activate(squibID, transform.position, Quaternion.identity); //shows normal vfx hit
+			hitPoints -= damage;
+
 
 		if (hitPoints < 1 && gameObject.activeSelf == true)
 		{
