27c9b064d19790d396ac2d370637a8eb573c8884
Pablo Leon-Luna <paleonluna@gmail.com>
2014-09-29 13:15:23 -0700

    fixed audio error, added public audio variable to firing

--- Assets/Source/Scripts/Firing/Firing.cs
+++ Assets/Source/Scripts/Firing/Firing.cs
@@ -15,7 +15,7 @@ public class Firing : MonoBehaviour {
 	public enum Direction {Up, Down};
 	public Direction direction;
 
-	AudioSource mainGun01;
+	public AudioSource mainWeapon;
 	//AudioSource secondWeapon01;
 
 		// This is a coroutine that gets kicked off...
@@ -28,7 +28,7 @@ public class Firing : MonoBehaviour {
 	}
 
 	void Start() {
-		if (direction == Direction.Down)
+		if (direction == Direction.Down) //if the ship is flying down or is an enemy, shoot facing down not up
 			projectileRotation.y += 180;
 		if (autoFire)
 			StartCoroutine ("UpdateFiring");
@@ -38,14 +38,13 @@ public class Firing : MonoBehaviour {
 	public virtual void FireProjectile() {
 
 		if (randomizeAim) {
-			angleOffset = Random.Range (-angleRandomness, angleRandomness);
+			angleOffset = Random.Range (-angleRandomness, angleRandomness);	//randomizes angle at which projectile exits ship
 		}
 		projectileRotation.y += angleOffset;
-		objectPool.Activate(projectileID, shotSpawn.position, Quaternion.Euler(projectileRotation/2));
+		objectPool.Activate(projectileID, shotSpawn.position, Quaternion.Euler(projectileRotation/2)); // activates projectile	
 		projectileRotation.y -= angleOffset;
 
-		AudioSource[] audios = GetComponents<AudioSource> ();
-		mainGun01 = audios [0];
-		mainGun01.Play();
+		if (mainWeapon != null)
+			mainWeapon.Play();//play sound of mainGun firing
 	}
 }
