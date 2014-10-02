71a39c7f989b2f4e24d504d55214a48ef69ed478
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-01 18:52:16 -0700

    press M to toggle pause menu when playing in Unity editor

--- Assets/Source/Scripts/Controllers/Pauser.cs
+++ Assets/Source/Scripts/Controllers/Pauser.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Pauser : MonoBehaviour {
 
-#if UNITY_EDITOR
-
+#if UNITY_EDITOR			
+	public Canvas Menu;
+	bool isMenu = true;
+	
+	void Update() {
+		if (Input.GetKeyDown ("m")) {
+			Menu.gameObject.SetActive(!isMenu);
+			isMenu = !isMenu;
+		}
+	}
 
 #elif UNITY_IPHONE
+	public Canvas pauseMenu;
 	//on event player puts down finger 1
 	//time scale = 1.0
 
@@ -29,12 +40,14 @@ public class Pauser : MonoBehaviour {
 	void PauseGame() {
 		Time.timeScale = 0.0f;
 		isPaused = true;
+		pauseMenu.gameObject.setActive(true);
 
 	}
 
 	void UnpauseGame() {
 		Time.timeScale = 1.0f;
 		isPaused = false;
+		pauseMenu.gameObject.setActive(false);
 
 	}
 
