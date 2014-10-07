031c1a4f89f9a10f272b18ddac540c81b2e8d995
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-07 01:01:40 -0400

    added in stephens ships - needs a lot of work still

--- Assets/Source/Scripts/Mover/StraightDownMover.cs
+++ Assets/Source/Scripts/Mover/StraightDownMover.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class StraightDownMover : Mover {
 	public override void Move() {
-		transform.Translate(new Vector3(0, 0, -1) * speed);
+		transform.Translate(new Vector3(0, 0, -0.1f) * speed);
 	}
 }
