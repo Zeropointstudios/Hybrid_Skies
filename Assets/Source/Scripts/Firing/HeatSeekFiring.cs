9180e63d0d7e1c1907c3fb947016558cb7b2bea6
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-08 18:02:37 -0400

    added missile

---
+++ Assets/Source/Scripts/Firing/HeatSeekFiring.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeatSeekFiring : Firing {
+
+	GameObject heatSeeker;
+	GameObject player;
+	bool seekEnemy = true;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	public virtual void FireProjectile() {
+
+		heatSeeker = Finder.GetObjectPool().Activate(projectileID, shotSpawn.position, Quaternion.Euler(projectileRotation/2)); // activates projectile	
+		print ("seek");
+		if (mainWeapon != null)
+			mainWeapon.Play();//play sound of mainGun firing
+		StartCoroutine ("Seek");
+	}
+
+	IEnumerator Seek(){
+		while(seekEnemy) {
+			heatSeeker.transform.LookAt (player.transform.position);
+			yield return new WaitForSeconds(0.1f);
+			print ("seeking");
+		}
+	}
+}
---
+++ Assets/Source/Scripts/Weapons/Modifiers/HeatSeekModifierEnemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HeatSeekModifierEnemy : MonoBehaviour {
+	
+	GameObject player;
+	bool isSeeking = true;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Start() {
+		StartCoroutine ("Seek");
+	}
+
+	IEnumerator Seek() {
+		if (player != null) 
+			yield return new WaitForSeconds (0.5f);
+			while (isSeeking) {
+				yield return new WaitForSeconds (0.5f);
+				gameObject.transform.LookAt(player.transform.position);
+		}
+	}
+}
