521713dd388ad148a3a66379837daa589ba0078a
Aaron Smith <magicsquare15@gmail.com>
2014-10-09 02:14:26 -0700

    HeatSeekModifier improved targeting.

--- Assets/Source/Scripts/Weapons/Modifiers/HeatSeekModifier.cs
+++ Assets/Source/Scripts/Weapons/Modifiers/HeatSeekModifier.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class HeatSeekModifier : MonoBehaviour {
 	
-	Vector3 target;
+	Vector3? target = null;
 	float counter = 0;
 
 	void Awake() {
@@ -19,15 +19,25 @@ public class HeatSeekModifier : MonoBehaviour {
 
 	void RadarSweep()
 	{
-		var transformArray = GameObject.FindGameObjectsWithTag("Enemy")
-			.Select(go => go.transform.position)
-				.OrderBy(t => Vector3.SqrMagnitude(t - transform.position))
-				.ToArray();
-		target = transformArray [0];
+		// Find the enemy that's closest.
+		GameObject bestEnemy = null;
+		float bestUtility = Mathf.NegativeInfinity;
+		foreach (GameObject enemy in Finder.GetEnemies()) {
+			// Lowest ratio of health gets healed.
+			float utility = -(Vector3.SqrMagnitude(enemy.transform.position - transform.position));
+			if (utility > bestUtility) {
+				bestUtility = utility;
+				bestEnemy = enemy;
+			}
+		}
+		if (bestEnemy != null)
+			target = bestEnemy.transform.position;
 	}
 
 	void Update() {
-		gameObject.transform.LookAt(target);
+		// Weird, I can comment this out and it still works.
+		if (target.HasValue)
+			gameObject.transform.LookAt(target.Value);
 		counter += Time.deltaTime;
 		if (counter > 2) { //find new target
 			RadarSweep();
