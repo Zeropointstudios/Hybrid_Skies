ec6b400c703033b2e492b52c63c742ebd57ecc79
Evan Hembacher <evanh3@gmail.com>
2014-10-08 20:49:37 -0700

    Fixed Pauser.cs to work with iOS

--- Assets/Source/Scripts/Controllers/Pauser.cs
+++ Assets/Source/Scripts/Controllers/Pauser.cs
@@ -1,58 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-
 
 public class Pauser : MonoBehaviour {
-
-	public GameObject Menu;
-
-#if UNITY_EDITOR			
-
-	bool isMenu = true;
 	
-	void Update() {
-		if (Input.GetKeyDown ("m")) {
-			Menu.gameObject.SetActive(!isMenu);
-			isMenu = !isMenu;
-		}
-	}
-
-#elif UNITY_IPHONE
-	public Canvas pauseMenu;
+	#if UNITY_EDITOR
+	
+	
+	#elif UNITY_IPHONE
 	//on event player puts down finger 1
 	//time scale = 1.0
-
+	
 	public static bool isPaused;
 	void Awake() {
 		Time.timeScale = 0.0f;
 		isPaused = true;
 	}
-
+	
 	void OnEnable() {
 		iPhonePauseHandler.Pause += PauseGame;
 		iPhonePauseHandler.Unpause += UnpauseGame;
 	}
-
+	
 	void OnDisable() {
 		iPhonePauseHandler.Pause -= PauseGame;
 		iPhonePauseHandler.Unpause -= UnpauseGame;
 	}
-
+	
 	void PauseGame() {
 		Time.timeScale = 0.0f;
 		isPaused = true;
-		Menu.setActive(true);
-
+		
 	}
-
+	
 	void UnpauseGame() {
 		Time.timeScale = 1.0f;
 		isPaused = false;
-		Menu.setActive(false);
-
+		
 	}
-
-#endif
-
+	
+	#endif
+	
 }
